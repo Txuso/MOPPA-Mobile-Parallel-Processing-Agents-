b74d4508bf4820afe91567d74d25079c2d1ecba7
Txuso <Txuso@192.168.0.203>
2015-10-28 12:58:59 +0100

    Task class

---
+++ MOPPA/src/main/java/classes/Task.java
@@ -0,0 +1,26 @@
+package classes;
+
+public class Task {
+	private double taskID;
+	private double taskValue;
+	private double taskResult;
+	public double getTaskID() {
+		return taskID;
+	}
+	public void setTaskID(double taskID) {
+		this.taskID = taskID;
+	}
+	public double getTaskValue() {
+		return taskValue;
+	}
+	public void setTaskValue(double taskValue) {
+		this.taskValue = taskValue;
+	}
+	public double getTaskResult() {
+		return taskResult;
+	}
+	public void setTaskResult(double taskResult) {
+		this.taskResult = taskResult;
+	}
+
+}
